59d26e2c323ddfee65ecfae194974352e5b20634
lenconstan <kruize.cl@gmail.com>
2021-09-20 20:43:12 +0200

    host in init

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# if __name__=="__main__":
-#     app.run(host='0.0.0.0', debug=False)
+if __name__=="__main__":
+    app.run(host='0.0.0.0', debug=False)
 
 from app import routes
